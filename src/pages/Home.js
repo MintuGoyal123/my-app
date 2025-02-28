38066191db6279510f2958c20e31aa9829e3326e
MintuGoyal123 <mintu.goyal87@gmail.com>
2025-02-28 14:44:00 +0530

    Initial Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import '../styles/main.css';
+import Banner1 from '../images/banner2.jpg';
 export function Home(){
     return(
         <div className="main-wrapper">
         <Header />
         <h2>My Home Component</h2>
+        <img src={Banner1} />
         <Footer />
         </div>
     );
